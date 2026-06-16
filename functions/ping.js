e61cc155eb33e3a7548deebac48c709a28112f87
kcyone01-cloud <kcyone01@gmail.com>
2026-06-17 00:05:00 +0800

    Add files via upload

--- functions/ping.js
+++ functions/ping.js
@@ -1,33 +1,32 @@
 // netlify/functions/ping.js
 exports.handler = async (event, context) => {
-    // We use a public, reliable URL to test reachability
-    const targetUrl = 'http://13.249.231.126';
-
+    const targetUrl = 'http://13.249.231.126'; // Target server to ping
+    
+    // 1. Measure latency to the target
+    const start = Date.now();
+    let status = "Unreachable";
     try {
-        const start = Date.now();
-        // Use 'HEAD' request to minimize bandwidth; set a timeout
-        const response = await fetch(targetUrl, { 
-            method: 'HEAD', 
-            signal: AbortSignal.timeout(2000) 
-        });
-        const end = Date.now();
-
-        return {
-            statusCode: 200,
-            headers: { 
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*" 
-            },
-            body: JSON.stringify({ status: "Online", time: `${end - start} ms` }),
-        };
-    } catch (error) {
-        return {
-            statusCode: 200,
-            headers: { 
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*" 
-            },
-            body: JSON.stringify({ status: "Unreachable", time: "Timeout/Error" }),
-        };
+        await fetch(targetUrl, { method: 'HEAD', signal: AbortSignal.timeout(2000) });
+        status = "Online";
+    } catch (e) {
+        status = "Unreachable";
     }
+
+    // 2. Proxy IP info to avoid CORS errors
+    let ipData = { ip: "N/A", org: "N/A" };
+    try {
+        const ipRes = await fetch('https://ipapi.co/json/');
+        ipData = await ipRes.json();
+    } catch (e) {}
+
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ 
+            status, 
+            time: `${Date.now() - start} ms`,
+            ip: ipData.ip,
+            isp: ipData.org
+        }),
+    };
 };
